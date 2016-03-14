2d5ade2001aea38c09a9d53f1b4b37e65f471f78
John Helly <j.c.helly@durham.ac.uk>
2016-03-14 16:14:36 +0100

    Added python example to plot millennium fof group

--- examples/millennium/python/plot_fof_group.py
+++ examples/millennium/python/plot_fof_group.py
@@ -3,16 +3,19 @@
 # Plot particles in the specified FoF group
 #
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 import virgo.sims.millennium as mill
-from virgo.utils.match import match
+from virgo.util.match import match
 
 # These specify the group to read in
 isnap = 63    # Snapshot number
 ifile = 0     # Which group_tab file the group is in
-ifof  = 0     # Position of group in the group_tab file, starting from zero
+ifof  = 10    # Position of group in the group_tab file, starting from zero
 
 # Read in group lengths and offsets from the group_tab file
-fname    = "/virgo/data/Millennium/snapshot_%03d/group_tab_%03d.%d" % (isnap, isnap, ifile)
+fname    = "/virgo/data/Millennium/snapdir_%03d/group_tab_%03d.%d" % (isnap, isnap, ifile)
 grouptab = mill.GroupTabFile(fname)
 fof_len    = grouptab["GroupLen"][...]
 fof_offset = grouptab["GroupOffset"][...]
@@ -22,7 +25,7 @@ fof_offset = grouptab["GroupOffset"][...]
 # from the ordering of IDs within each FoF group)
 fname  = "/virgo/data/Millennium/postproc_%03d/sub_ids_%03d.%d" % (isnap, isnap, ifile)
 subids = mill.SubIDsFile(fname)
-ids    = subids[fof_offset[ifof]:fof_offset[ifof]+fof_len[ifof]] # Read just the IDs for this FoF group
+ids    = subids["GroupIDs"][fof_offset[ifof]:fof_offset[ifof]+fof_len[ifof]] # Read just the IDs for this FoF group
 
 # IDs in the sub_ids file consist of particle ID in the 34 least
 # significant bits and hash keys in the 30 most significant bits.
@@ -30,24 +33,38 @@ ids    = subids[fof_offset[ifof]:fof_offset[ifof]+fof_len[ifof]] # Read just the
 hash_key = np.right_shift(ids, 34)
 ids      = np.right_shift(np.left_shift(ids,30), 30)
 
+# hash_key contains hash key for every particle in the FoF group.
+# Make array of unique hash keys to read.
+keys_to_read = np.unique(hash_key)
+
 # Now read the particle from the hash cells corresponding to these keys
 snap = mill.Snapshot("/virgo/data/Millennium/", "snap_millennium", isnap)
-pos, vel, snap_ids = snap.read_cells(hash_key)
+snap_pos, snap_vel, snap_ids = snap.read_cells(keys_to_read)
 
 # Then we need to determine which of these particles belong to
 # the FoF group we're interested in. For each particle ID in the
 # sub_ids file we want to find the corresponding ID in the set of
 # particles we just read in.
 ptr = match(ids, snap_ids) # For each entry in ids returns index of same value in snap_ids
+                           # or -1 where there's no match
 
 # Check we found all the particles
 if np.any(ptr<0):
     raise Exception("Failed to find particle in specified group!")
 
-# All particle IDs from sub_ids should match an entry in snap_ids
+# Check we matched up the IDs correctly
 if np.any(ids != snap_ids[ptr]):
     raise Exception("Particle IDs do not match!")
 
 # Find positions and velocities of particles in the FoF group
-pos = pos[ptr,:]
-vel = vel[ptr,:]
+pos = snap_pos[ptr,:]
+vel = snap_vel[ptr,:]
+
+# Make a dotplot
+plt.plot(pos[:,0], pos[:,1], "k.")
+plt.gca().set_aspect("equal")
+plt.xlabel("x (comoving Mpc/h)")
+plt.ylabel("y (comoving Mpc/h)")
+plt.title("FoF group %d from file %d of snapshot %d" % (ifof, ifile, isnap))
+plt.show()
+
